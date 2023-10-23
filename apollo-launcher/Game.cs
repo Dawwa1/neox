f7b1ee0769e04f239bc3760a74838d4013be4c79
vovan <vovan@DESKTOP-GN617LG>
2023-10-23 06:02:56 -0500

    error: line 82 mainwindow.cs

---
+++ apollo-launcher/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows;
+
+namespace apollo_launcher
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Icon { get; set; }
+        public Game(string name, string path, string icon = null)
+        {
+            Name = name;
+            Path = path;
+            Icon = icon;
+        }
+
+        public void launchGame()
+        {
+            if (MessageBox.Show("Are you sure you want to launch " + Path, "Launch game", MessageBoxButton.YesNo) == MessageBoxResult.OK)
+            {
+                Process.Start(Path);
+            } else
+            {
+                // nothing
+            }
+        }
+    }
+}
--- apollo-launcher/MainWindow.xaml.cs
+++ apollo-launcher/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static apollo_launcher.Game;
 
 namespace apollo_launcher
 {
@@ -20,6 +25,9 @@ namespace apollo_launcher
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public List<Game> games = new List<Game>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -41,6 +49,54 @@ namespace apollo_launcher
             {
                 this.DragMove();
             }
+        }
+
+        private void addGame_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            ofd.Filter = "Executables (*.exe)|*.exe";
+            ofd.Multiselect = false;
+
+            string[] name_split = ofd.FileName.Split("\\");
+            //string name = "../" + name_split[name_split.Length - 2] + "/" + name_split[name_split.Length - 1];
+
+            string name = name_split[name_split.Length - 1];
+            string path = ofd.FileName;
+            Game game = new Game(name, path);
+            games.Add(game);
+
+            // Add to column/row
+            addGameToView(game);
+        }
+
+        private void addGameToView(Game game)
+        {
+            Button button = new Button();
+            if (game.Icon == null)
+            {
+                button.Content = game.Name;
+
+                if (gamesGrid.Children.Count > 0) {
+                    foreach (UIElementCollection child in gamesGrid.Children)
+                    {
+                        foreach (UIElement element in child)
+                        {
+                            Debug.Write(element.ToString());
+                        }
+                    }
+                } else
+                {
+                    Grid.SetColumn(button, 0);
+                    Grid.SetRow(button, 0);
+                    gamesGrid.Children.Add(button);
+                }
+            }
+        }
+
+        private void onImage_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
